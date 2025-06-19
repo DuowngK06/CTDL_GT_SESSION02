db1eceeeff5356bb8c706122b8bebd91d1c4192e
DuowngK06 <dh5147666@gmail.com>
2025-06-19 19:55:00 +0700

    Add files via upload

--- CNTT3_IT104_SS2_Bai2.cpp
+++ CNTT3_IT104_SS2_Bai2.cpp
@@ -1,36 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+void imporArray(int **array, int *quantity) {
+    printf("nhap so luong phan tu : ");
+    scanf("%d", quantity);
+
+    if (*quantity <= 0 || *quantity > 100) {
+        printf("So luong ko hop le\n");
+        return;
+    }
+
+    *array = (int *)malloc(*quantity * sizeof(int));
+    if (*array == NULL) {
+        printf("bo nho ko du de thuc hien thao tac nay\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < *quantity; i++) {
+        printf("Nhap phan tu mang[%d]: ", i);
+        scanf("%d", &(*array)[i]);
+    }
+    
 
 int main() {
-    int n, x, i;
-    int arr[100];
-    int count = 0;
+    int *array = NULL;
+    int quantity;
+    imporArray(&array, &quantity);
 
-
-    do {
-        printf("Nhap so luong phan tu (1 - 100): ");
-        scanf("%d", &n);
-    } while (n <= 0 || n > 100);
-
-
-    for (i = 0; i < n; i++) {
-        printf("arr[%d] = ", i);
-        scanf("%d", &arr[i]);
-    }
-
-
-    printf("Nhap so can kiem tra: ");
-    scanf("%d", &x);
-
-  
-    for (i = 0; i < n; i++) {
-        if (arr[i] == x) {
-            count++;
-        }
-    }
-
-  
-    printf("So lan xuat hien cua %d la: %d\n", x, count);
-
+    free(array);
     return 0;
 }
 
